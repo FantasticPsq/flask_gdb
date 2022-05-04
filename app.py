bb4b78f0baf65ac840dd99d7d6a655bcf84178ee
FantasticPsq <1636538091@qq.com>
2022-05-04 21:25:16 +0800

    v

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import os.path
 from flask import Flask, request
 from werkzeug.utils import secure_filename
 
-from Variable import Variable
-
 app = Flask(__name__)
 gdb = importlib.import_module("gdb")
 bash_path = os.path.dirname(__file__)
@@ -162,6 +160,68 @@ def get_variable_by_expression(expression):
         return None
     return variable
 
+
+class Variable(gdb.Variable):
+
+    def __int__(self, frame, symbol=False, value=False, expression=False):
+        self.frame = frame
+        self.symbol = symbol
+        self.value = value
+        self.expression = expression
+        if self.expression:
+            self.name = self.expression.split(".")[-1].split("->")[-1]
+        else:
+            self.name = self.symbol.name
+
+    def serializable(self):
+
+        frame = self.frame
+        symbol = self.symbol
+        if symbol:
+            value = symbol.value(frame)
+        else:
+            value = self.value
+
+        try:
+            block = frame.block()
+        except RuntimeError as e:
+            print(e)
+            return False
+        serializable = {}
+        serializable["is_global"] = block.is_global
+        serializable["name"] = self.name
+        serializable["expression"] = self.expression
+        serializable["is_pointer"] = value.type.code == gdb.TYPE_CODE_PTR
+        serializable["is_optimized_out"] = value.is_optimized_out
+        try:
+            serializable["address"] = str(value.address) if value.address else "0x0"
+        except:
+            serializable["address"] = "0x0"
+
+        try:
+            if not value.is_optimized_out:
+                serializable["value"] = value.lazy_string(length=10000).value().string()
+                serializable["is_nts"] = True
+            else:
+                serializable["value"] = '<optimized out>'
+                serializable["is_nts"] = False
+        except gdb.error as e:
+            try:
+                serializable["is_nts"] = False
+                serializable["value"] = str(value)
+            except gdb.MemoryError as e:
+                print(e)
+                return None
+            except gdb.error as e:
+                print(e)
+        except UnicodeDecodeError as e:
+            serializable["is_nts"] = False
+            serializable["value"] = str(value)
+        except Exception as e:
+            print(e)
+
+            return None
+        return serializable
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
